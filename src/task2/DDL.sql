ac82b7a943855cffcfb7f6cfa4948c7473d80a55
ElyAi <12211308@mail.sustech.edu.cn>
2023-10-12 20:34:04 +0800

    modify sql code and database diagram

--- src/task2/DDL.sql
+++ src/task2/DDL.sql
@@ -1,11 +1,11 @@
 create table project_user
 (
-    mid         integer not null
+    mid         bigint not null
         constraint user_pkey
             primary key,
     name        varchar(400) not null ,
     sex         varchar(10),
-    birthday    date,
+    birthday    varchar(20),
     level       integer not null ,
     sign        varchar(2000),
     identity    varchar(20) not null
@@ -17,14 +17,14 @@ create table project_videos
         constraint pk_videos
             primary key ,
     title       varchar(400),
-    owner_mid   integer
+    owner_mid   bigint
         references project_user (mid),
     commit_time timestamp,
     review_time timestamp,
     public_time timestamp,
     duration    integer not null ,
     description varchar(2000),
-    reviewer    integer
+    reviewer    bigint
         references project_user (mid)
 );
 
@@ -32,18 +32,18 @@ create table project_danmu
 (
     BV          varchar(400)
         references project_videos (BV),
-    mid         integer
+    mid         bigint
         references project_user (mid),
-    time        varchar(400),
+    time        float4,
     content     varchar(400),
     primary key (BV, mid, time)
 );
 
 create table project_following
 (
-    user_mid    integer
+    user_mid    bigint
         references project_user(mid),
-    fans_mid    integer
+    fans_mid    bigint
         references project_user(mid),
     primary key (user_mid, fans_mid)
 );
@@ -52,7 +52,7 @@ create table project_like
 (
     BV          varchar(400)
         references project_videos (BV),
-    mid         integer
+    mid         bigint
          references project_user (mid),
     primary key (BV, mid)
 );
@@ -61,7 +61,7 @@ create table project_coin
 (
     BV          varchar(400)
         references project_videos (BV),
-    mid         integer
+    mid         bigint
          references project_user (mid),
     primary key (BV, mid)
 );
@@ -70,7 +70,7 @@ create table project_favorite
 (
     BV          varchar(400)
         references project_videos (BV),
-    mid         integer
+    mid         bigint
          references project_user (mid),
     primary key (BV, mid)
 );
@@ -79,8 +79,9 @@ create table project_view
 (
     BV          varchar(400)
         references project_videos (BV),
-    mid         integer
+    mid         bigint
         references project_user (mid),
     time        integer,
     primary key (BV, mid)
 );
+
